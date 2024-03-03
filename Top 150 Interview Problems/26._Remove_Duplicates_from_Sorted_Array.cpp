b8141881a1c44444efd78ece47378a81b5caf040
tvtsolov <65929968+tvtsolov@users.noreply.github.com>
2024-03-03 21:06:40 +0200

    task 4 solved

--- Top 150 Interview Problems/26._Remove_Duplicates_from_Sorted_Array.cpp
+++ Top 150 Interview Problems/26._Remove_Duplicates_from_Sorted_Array.cpp
@@ -10,21 +10,23 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) { 
-        vector<int>::iterator it;
-        it = std::unique (nums.begin(), nums.end());
-
-        nums.erase(it, nums.end()); 
-
-        return nums.size();
+        int insertindex = 1;
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(nums[i-1] != nums[i])
+            {
+                nums[insertindex]=nums[i];
+                insertindex++;
+            }
+        }
+        return insertindex;
     }
 };
 
 int main(){
-    vector<int> vec = {2,2,4,5,66,66,71,72,73,100,100};
+    vector<int> vec = {2,5,66,66,66,66,990};
     Solution sol;
     int k = sol.removeDuplicates(vec);
-
-
 
 }
 
